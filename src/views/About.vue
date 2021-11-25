b5c77d292bed69acd5a61f8a324d233893931202
funkoke <michaelyenoke@gmail.com>
2021-11-25 15:55:13 +0800

    update after delete button

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about" pa-6>
+    <h1>This is an about page</h1>
+
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam quae optio vero enim, sed ipsum, illum iure dignissimos iusto ea voluptas unde hic. Temporibus commodi corporis facilis perferendis illum!</p>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam quae optio vero enim, sed ipsum, illum iure dignissimos iusto ea voluptas unde hic. Temporibus commodi corporis facilis perferendis illum!</p>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam quae optio vero enim, sed ipsum, illum iure dignissimos iusto ea voluptas unde hic. Temporibus commodi corporis facilis perferendis illum!</p>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam quae optio vero enim, sed ipsum, illum iure dignissimos iusto ea voluptas unde hic. Temporibus commodi corporis facilis perferendis illum!</p>
+  </div>
+</template>
